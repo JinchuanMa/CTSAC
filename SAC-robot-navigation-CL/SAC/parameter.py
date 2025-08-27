2a831df925de3a370aab07e0f5f947e5930b94e6
JinchuanMa <102812412+JinchuanMa@users.noreply.github.com>
2025-08-27 16:46:20 +0800

    Update parameter.py

--- SAC-robot-navigation-CL/SAC/parameter.py
+++ SAC-robot-navigation-CL/SAC/parameter.py
@@ -10,8 +10,8 @@ def get_parameters():
     parser.add_argument('--initial_tem', default=1, type=int)                 #Initial temperature coefficient
     parser.add_argument('--tem_decay_rate', default=0.0, type=int)            #Decay rate
 
-    parser.add_argument('--learning_rate', default=5e-4, type=int)            
-    parser.add_argument('--gamma', default=0.98, type=int)                    # discount gamma
+    parser.add_argument('--learning_rate', default=5e-4, type=float)            
+    parser.add_argument('--gamma', default=0.98, type=float)                    # discount gamma
     parser.add_argument('--capacity', default=1e5, type=int)                  # replay buffer size
     parser.add_argument('--iteration', default=5000000, type=int)             # num of  games(episode)
     parser.add_argument('--batch_size', default=256, type=int)                # mini batch size
@@ -21,12 +21,12 @@ def get_parameters():
     parser.add_argument('--log_interval', default=300, type=int)              # Save the model once in episode
     parser.add_argument('--load', default=False, type=bool)                   # load model
     parser.add_argument('--save', default=True, type=bool)                    # save model
-    parser.add_argument('--expl_noise', default=0.3, type=int)                # Initial exploration noise starting value in range [expl_min ... 1]
-    parser.add_argument('--expl_min', default=0.05, type=int)                 # Exploration noise after the decay in range [0...expl_noise]
+    parser.add_argument('--expl_noise', default=0.3, type=float)                # Initial exploration noise starting value in range [expl_min ... 1]
+    parser.add_argument('--expl_min', default=0.05, type=float)                 # Exploration noise after the decay in range [0...expl_noise]
     parser.add_argument('--expl_decay_steps', default=10000, type=int)        # Number of steps over which the initial exploration noise will decay over
     parser.add_argument('--robot_num', default=1, type=int)                   # number of robots
     parser.add_argument('--random_robot_position', default=True, type=bool)   # robot's initial position is random
     parser.add_argument('--robot_position_x', default=1, type=int)            # Robot initial position
     parser.add_argument('--robot_position_y', default=1, type=int)            # Robot initial position
-    parser.add_argument('--distance_threshold', default=0.4, type=int)        # Robot repetitive position judgment threshold
+    parser.add_argument('--distance_threshold', default=0.4, type=float)        # Robot repetitive position judgment threshold
     return parser
